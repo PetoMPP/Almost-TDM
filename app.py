261d75f3c675e9ce48ced1dff40674c8eb65592b
PetoMPP <piotreq22@gmail.com>
2021-10-31 12:25:06 +0100

    gitignore fixes

--- app.py
+++
@@ -1,7 +0,0 @@
-import win32com.client as win32
-
-outlook = win32.Dispatch('outlook.application')
-mail = outlook.CreateItem(0)
-mail.To = "pietrzyk.p@axito.pl"
-mail.Subject = "Problem/Sugestia dotycząca programu Almost TDM"
-mail.Display(True)
